e18189fc9674f284f35681d029ba12648e861447
Jacob Buck <123811017+SquidMonkey16@users.noreply.github.com>
2025-11-25 19:32:47 -0800

    Add complete sidenav functionality

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const sidebarMenus = document.querySelectorAll(".sidebar a");
+const topNav = document.querySelector(".top-nav");
+const sideNavMenus = document.querySelectorAll(".side-nav a");
+const sideNavDropdowns = document.querySelectorAll(".side-nav .dropdown");
+const sideNavLinks = document.querySelectorAll(".side-nav a");
+const clickedLinkIndex = localStorage.getItem('active_link');
+const dropdownButtons = document.querySelectorAll(".dropdown button");
 
-sidebarMenus.forEach(menu => {
+// Animate top-nav on window load
+window.addEventListener("load", () => {
+    topNav.classList.add("show-bg");
+});
+
+sideNavMenus.forEach(menu => {
     menu.addEventListener("mouseover", (event) => {
         const li = event.target.closest("li");
         li.classList.add("hovered");
@@ -12,32 +22,46 @@ sidebarMenus.forEach(menu => {
     });
 });
 
-const sidebarDropdowns = document.querySelectorAll(".sidebar .dropdown");
-
-sidebarDropdowns.forEach(dropdown => {
+sideNavDropdowns.forEach(dropdown => {
     const button = dropdown.querySelector("button");
 
     button.addEventListener("click", () => {
         dropdown.classList.toggle("active");
+        dropdown.nextElementSibling.classList.toggle("show");
     });
 });
 
-const sidebarLinks = document.querySelectorAll(".sidebar a");
-
-sidebarLinks.forEach(a => {
+sideNavLinks.forEach((a, index) => {
     a.addEventListener("click", () => {
-        localStorage.setItem("active_link", a.textContent);
-        console.log("a.textContent is ", a.textContent); // delete
+        localStorage.setItem("active_link", index);
     });
+
+    // Set the href of every sidebar link to /pages/placeholder.html, except for "Home" and "Program Accreditation & Assessment"
+    if (a.textContent !== "Home" && a.textContent !== "Program Accreditation & Assessment") {
+        a.href = "/pages/placeholder.html";
+    }
 });
 
-const clickedLink = localStorage.getItem('active_link');
 
-if (clickedLink) {
-    const links = Array.from(document.querySelectorAll(".sidebar a"));
-    const activeLink = links.filter(a => a.textContent === clickedLink)[0];
-    
-    if (links) {
-        activeLink.parentElement.classList.add('active');
+const links = Array.from(document.querySelectorAll(".side-nav a"));
+const activeLink = sideNavLinks[clickedLinkIndex];
+const ancestorDropdown = activeLink.closest(".dropdown");
+
+if (links) {
+    activeLink.parentElement.classList.add('active');
+
+    if (ancestorDropdown && ancestorDropdown.nextElementSibling.classList.contains("expandable")) {
+        ancestorDropdown.nextElementSibling.classList.add("show");
+    }
+
+    if (activeLink.closest(".expandable")) {
+        activeLink.closest(".expandable").classList.add("show");
+    }
+
+    let expandableDiv = activeLink.closest(".expandable");
+
+    while (expandableDiv !== null) {
+        expandableDiv.classList.add("show");
+        expandableDiv = expandableDiv.parentElement.closest(".expandable");
     }
 }
