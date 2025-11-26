e54b2a4181f194fe4242197ab271725871952594
Jacob Buck <123811017+SquidMonkey16@users.noreply.github.com>
2025-11-26 09:58:25 -0800

    Add content to accreditation page

--- main.js
+++ main.js
@@ -65,3 +65,89 @@ if (links) {
         expandableDiv = expandableDiv.parentElement.closest(".expandable");
     }
 }
+
+// Line chart
+const years = ["2009","2010","2011","2012","2013","2014","2015","2016","2017"];
+const total = [481,455,638,751,810,818,669,922,928];
+const fullTime = [406,401,584,666,729,708,599,864,862];
+const partTime = [75,54,54,85,81,110,70,58,66];
+
+const lineChart = new Chart("line-chart", {
+  type: "line",
+  data: {
+    labels: years,
+    datasets: [
+        {
+            label: "Total Students",
+            backgroundColor:"#FFB81C",
+            borderColor: "#FFB81C",
+            data: total
+        },
+        {
+            label: "Full Time Students",
+            backgroundColor:"#FFB81C",
+            borderColor: "#A4D65E",
+            data: fullTime
+        },
+        {
+            label: "Part Time Students",
+            backgroundColor:"#FFB81C",
+            borderColor: "#A4D65E",
+            data: partTime
+        }
+    ]
+  },
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Year', // X-axis label
+          color: '#FFB81C',
+          font: {
+            size: 18,
+            weight: 'bold'
+          }
+        },
+        ticks: {
+            color: "#A4D65E"
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Number of Students', // Y-axis label
+          color: '#FFB81C',
+          font: {
+            size: 18,
+            weight: 'bold'
+          }
+        },
+        ticks: {
+            color: "#A4D65E"
+        }
+      }
+    },
+    plugins: {
+        title: {
+            display: true,
+            text: 'Historic Undergraduate Enrollment and Graduation Data',
+            color: '#FFB81C',
+            font: {
+            size: 24,
+            },
+            padding: {
+            top: 10,
+            bottom: 10
+            }
+        },
+        legend: {
+            labels: {
+                color: "#A4D65E"
+            }
+        }
+    
+    }
+  }
+  
+});
